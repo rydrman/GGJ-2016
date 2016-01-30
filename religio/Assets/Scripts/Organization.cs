5fc2546361820ecb4bc12801f7aab6921a2a5a69
Ryan Bottriell <ryanbottriell@live.ca>
2016-01-30 13:46:06 -0800

    add topic enum and organization base

--- religio/Assets/Scripts/Organization.cs
+++ religio/Assets/Scripts/Organization.cs
@@ -1,12 +1,36 @@
 ﻿using System;
+using UnityEngine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
 
-namespace AssemblyCSharp
+[System.Serializable]
+public class Organization : MonoBehaviour
 {
-	public class Organization
+	
+	Dictionary<Topic, int> m_stances;
+
+	public Organization ()
 	{
-		public Organization ()
-		{
+		Topic[] topics = Topic.GetValues (Topic);
+		foreach(Topic t in topics) {
+			m_stances [t] = 0;
 		}
 	}
+
+	public int GetStance(Topic topic) {
+		return m_stances [topic];
+	}
+
+	public void SetStance(Topic topic, int value) {
+		m_stances [topic] = Math.Min(100, Math.Max(-100, value));
+	}
+
+	public int ChangeStance(Topic topic, int delta) {
+		int value = GetStance (topic) + delta;
+		SetStance (topic, value);
+		return value;
+	}
 }
+
 
--- religio/Assets/Scripts/Topic.cs
+++ religio/Assets/Scripts/Topic.cs
@@ -2,31 +2,11 @@
 using UnityEngine;
 
 [System.Serializable]
-public class Topic : MonoBehaviour {
-
-	static uint next_uid = 0;
-
-	uint m_uid;
-	int m_value = 0;
-
-	Topic() {
-		m_uid = next_uid++;
-		value = 0;
-	}
-
-	public uint uid {
-		get {
-			return m_uid;
-		}
-	}
-
-	public int value { 
-		get {
-			return m_value;
-		}
-		set {
-			m_value = Math.Max (-100, Math.Min (100, value));
-		}
-	}
-
+public enum Topic{
+	Education,
+	Culture,
+	Diplomacy,
+	Enterprise,
+	Freedom,
+	Welfare
 }
