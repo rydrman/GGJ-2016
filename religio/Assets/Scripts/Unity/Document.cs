1d72941370f0ccf9bab89aea46c102438641952d
Aaron Morris <aaorris@live.com>
2016-01-30 18:55:11 -0800

    adding cleaner animation and Document text setting

---
+++ religio/Assets/Scripts/Unity/Document.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Document : MonoBehaviour {
+
+	public UnityEngine.UI.Text document_title;
+	public UnityEngine.UI.Text document_body;
+	public string m_title = "TitleText";
+	public string m_body = "BodyText";
+
+	// Use this for initialization
+	void Start () {
+		SetTitle(m_title);
+		SetBody(m_body);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void SetTitle(string text) {
+		Debug.Log(text);
+		m_title = text;
+		document_title.text = text;
+	}
+
+	public void SetBody(string text) {
+		Debug.Log(text);
+		m_body = text;
+		document_body.text = text;
+	}
+}
