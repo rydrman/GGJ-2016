3cfee7b0720390357df5bba7e8e733107aaab890
Ryan Bottriell <ryanbottriell@live.ca>
2016-01-30 17:31:58 -0800

    Controllers and prefab loading

---
+++ religio/Assets/Scripts/Data/Decision.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using UnityEngine;
+
+[System.Serializable]
+public class Decision : MonoBehaviour {
+
+	public DecisionOption[] options = new DecisionOption[3];
+
+	string m_title;
+	DecisionOption m_choice;
+
+	public Decision( ) {
+	}
+
+	public DecisionOption choice{ 
+		get {
+			return m_choice;
+		}
+	}
+
+	public void Copy(Decision source) {
+		m_title = source.m_title;
+		options = source.options;
+	}
+
+	public void MakeDecision(DecisionOption option) {
+		if (-1 == Array.IndexOf(options, option)) {
+			return;
+		}
+		m_choice = option;
+	}
+
+}
--- religio/Assets/Scripts/DecisionSet.cs
+++
@@ -1,8 +0,0 @@
-﻿using UnityEngine;
-using System.Collections.Generic;
-
-public class DecisionSet {
-	public List<GameObject> newspapers = new List<GameObject> ();
-	public List<GameObject> memos = new List<GameObject> ();
-	public List<GameObject> docets = new List<GameObject> ();
-}
